c9914046adfc24505cf6b3990a74d8f61e92752a
M Afzaal Afzal <afzaal1721@gmail.com>
2025-02-06 17:10:24 +0100

    feat: fix bug

--- src/components/HomePage/Team/BuyWithoutFearInternal.tsx
+++ src/components/HomePage/Team/BuyWithoutFearInternal.tsx
@@ -8,7 +8,7 @@ export const BuyWithoutFearInternal = () => {
       <p className="text-center text-[48px] font-vallejo uppercase leading-[1] max-w-[230px] md:max-w-[unset] mx-auto">
         Buy without Fear
       </p>
-      <div className="bg-black/10 py-6 border-white border rounded-xl mt-4 md:mt-7 font-vallejo text-[24px] max-w-[500px] mx-auto ">
+      <div className=" rounded-xl mt-4 md:mt-7 font-vallejo text-[24px] max-w-[600px] mx-auto ">
         <div className="flex items-center justify-center gap-5 ">
           <Link
             className="w-[131px] h-[44px] bg-red text-black border text-center flex items-center justify-center border-white rounded-lg"
@@ -24,10 +24,7 @@ export const BuyWithoutFearInternal = () => {
           </Link>
         </div>
         <div className="px-4 mt-4">
-          <p className="text-sm text-center">Ca: 0xeEC37AB9bb9058bEEb0B4f931f685Bb74fC42EeA</p>
-          <p className="text-center py-2 max-w-[120px] mx-auto rounded-lg mt-3 border border-white">
-            $AKUMA{" "}
-          </p>
+          <p className="text-lg text-center">Ca: 0xeEC37AB9bb9058bEEb0B4f931f685Bb74fC42EeA</p>
         </div>
       </div>
     </div>

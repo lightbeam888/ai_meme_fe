e56eb4cf528b6342bb5c8c8ad7cba7860288f35c
M Afzaal Afzal <afzaal1721@gmail.com>
2025-02-06 17:59:33 +0100

    feat: update links

--- src/components/HomePage/Team/Royalty.tsx
+++ src/components/HomePage/Team/Royalty.tsx
@@ -1,26 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const team1 = [
   {
     id: "bored",
     name: "BORED",
-    imgSrc: "/team/royalties/1.jpeg"
+    imgSrc: "/team/royalties/1.jpeg",
+    href: "https://x.com/boredelonmusk?s=21"
   },
   {
     id: "hunter orrell🔥",
     name: "hunter orrell🔥",
-    imgSrc: "/team/royalties/2.jpeg"
+    imgSrc: "/team/royalties/2.jpeg",
+    href: "https://x.com/hunterorrell?s=21"
   },
   {
     id: "mattwright.eth | d/acc",
     name: "mattwright.eth | d/acc",
-    imgSrc: "/team/royalties/3.jpeg"
+    imgSrc: "/team/royalties/3.jpeg",
+    href: "https://x.com/mateo_ventures?s=21"
   },
   {
     id: "Quigs.base.eth",
     name: "Quigs.base.eth",
-    imgSrc: "/team/royalties/4.jpeg"
+    imgSrc: "/team/royalties/4.jpeg",
+    href: "https://x.com/quigleynft?s=21"
   }
 ];
 
@@ -28,22 +33,26 @@ const team2 = [
   {
     id: "RENDR | rendə(r)",
     name: "RENDR | rendə(r)",
-    imgSrc: "/team/royalties/5.jpeg"
+    imgSrc: "/team/royalties/5.jpeg",
+    href: "https://x.com/realrendr?s=21"
   },
   {
     id: "Vitalis (cute/acc)",
     name: "Vitalis (cute/acc)",
-    imgSrc: "/team/royalties/6.jpeg"
+    imgSrc: "/team/royalties/6.jpeg",
+    href: "https://x.com/vitaliselkins?s=21"
   },
   {
     id: "Christopher Kocurek",
     name: "Christopher Kocurek",
-    imgSrc: "/team/royalties/7.jpeg"
+    imgSrc: "/team/royalties/7.jpeg",
+    href: "https://x.com/bigironchris?s=21"
   },
   {
     id: "fity.eth",
     name: "fity.eth",
-    imgSrc: "/team/royalties/8.jpeg"
+    imgSrc: "/team/royalties/8.jpeg",
+    href: "https://x.com/fityeth?s=21"
   }
 ];
 
@@ -58,7 +67,12 @@ export const Royalty = () => {
           <div className="max-w-[845px] grid grid-cols-4 gap-6 md:gap-x-28 md:gap-y-10 flex-wrap">
             {team1.map((val, index) => {
               return (
-                <div key={index} className="flex flex-col items-center">
+                <Link
+                  href={val.href}
+                  target="_blank"
+                  key={index}
+                  className="flex flex-col items-center"
+                >
                   <Image
                     className="max-w-[90px] w-full md:max-w-[131px]"
                     src={val.imgSrc}
@@ -69,14 +83,19 @@ export const Royalty = () => {
                   <p className="mt-[18px] text-center font-vallejo text-sm md:text-[21px]">
                     {val.name}
                   </p>
-                </div>
+                </Link>
               );
             })}
           </div>
           <div className="mt-12  grid grid-cols-4 max-w-[845px] ml-auto gap-6 md:gap-x-28 md:gap-y-10 flex-wrap">
             {team2.map((val, index) => {
               return (
-                <div key={index} className="flex flex-col items-center">
+                <Link
+                  href={val.href}
+                  target="_blank"
+                  key={index}
+                  className="flex flex-col items-center"
+                >
                   <Image
                     className="max-w-[90px] w-full md:max-w-[131px]"
                     src={val.imgSrc}
@@ -87,7 +106,7 @@ export const Royalty = () => {
                   <p className="mt-[18px] text-center font-vallejo text-sm md:text-[21px]">
                     {val.name}
                   </p>
-                </div>
+                </Link>
               );
             })}
           </div>
@@ -96,7 +115,12 @@ export const Royalty = () => {
           <div className="mt-12  grid grid-cols-3 max-w-[845px] ml-auto gap-6 md:gap-x-28 md:gap-y-10 flex-wrap">
             {[...team1, ...team2].map((val, index) => {
               return (
-                <div key={index} className="flex flex-col items-center">
+                <Link
+                  target="_blank"
+                  href={val.href}
+                  key={index}
+                  className="flex flex-col items-center"
+                >
                   <Image
                     className="max-w-[90px] w-full md:max-w-[131px]"
                     src={val.imgSrc}
@@ -107,7 +131,7 @@ export const Royalty = () => {
                   <p className="mt-[18px] text-center font-vallejo text-sm md:text-[21px]">
                     {val.name}
                   </p>
-                </div>
+                </Link>
               );
             })}
           </div>
